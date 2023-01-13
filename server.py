f4bacfe9ecd0180149635b87e2933f72fe35b5eb
Hesam760 <ghasemi.hesam3@gmail.com>
2023-01-13 19:53:14 +0330

    adding Chat and fix GUI bug

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import json
 import socket
 import time
+import os
 from _thread import *
 from datetime import datetime
+import threading
 
 # get the hostname
 host = socket.gethostname()
@@ -32,47 +34,109 @@ for i in range(0, len(dataFile)):
 
 list_client_name = []
 
-
+clients={}
+    
+def timer(time1):
+    global state
+    state = True
+    while True:
+        if time.time() > time1: 
+            state = False
+            break
+        # print(time.time() - time1)
+        time.sleep(1)
+        
 def progress(conn, addr, score_board):
-
-    username = conn.recv(1024).decode()
-    list_client_name.append(username)
+    print(listOfConnections)
+    progress.username = conn.recv(1024).decode()
+    list_client_name.append(progress.username)
 
     # send number of question
     conn.send(str(len(dataFile)).encode())
 
     while True:
-        if len(list_client_name) == 1:
+        if len(list_client_name) == 3:
             for i in range(0, len(dataFile)):
                 # send question
                 
                 localtime1 = time.time()
                 print("t1:", localtime1)
                 question = json.dumps(data2[i]).encode()
+                
                 conn.send(question)
                 
                 # receive answer
                 ans = conn.recv(1024).decode()
-        
+                print(ans)
+                
                 localtime2 = time.time()
                 print("t2:" , localtime2)
                 timestamp = localtime2 - localtime1
                 print("t3:", timestamp)
                 
-                if ans != 'TimeOut':
-                    # conn.send("waiting for clients".encode())
-                    time.sleep(abs(45-timestamp))
+                
+                if ans != 'TimeOut': 
+                    if ans == str(dataFile[i]['answer']):
+                        score_board[addr[1]] += 1
+                                       
+                    times = 45 - timestamp 
+                    newtime = time.time() +  times
+                    timerThread = threading.Thread(target=timer, args=(newtime,))
+                    # timerThread.start()
+                    
+                    chat = ''
+                    def input_and_send():
+                        global chatList
+                        global stop_thread
+                        stop_thread = False
+                        chatList = []
+                        lastChat = ''
+                        while 1:
+                            if len(chatList) == 1:
+                                for i in chatList:
+                                    for client in listOfConnections:
+                                        if(str(client) != str(conn)):
+                                            client.send(i.encode())
+                                            print("sending " , i)
+                                    chatList.clear()
+                                    break
+                                
+                            if time.time() > newtime :
+                                # lastChat = i
+                                # print("lastchat:",lastChat)
+                                break
 
-                #     #if>30 time is up.
-                # if timestamp > 10:
-                #     print ("Your time is up!\n")
-                #     conn.close()
-                if ans == str(dataFile[i]['answer']):
-                    score_board[addr[1]] += 1
-                    # send score board
+                                  
+                    background_thread = threading.Thread(target=input_and_send)
+                    # background_thread.daemon = True
+                    background_thread.start()
+
+                    while True :
+                        chat = conn.recv(1024).decode()
+                        if(chat != ''):
+                            print("recv chat:" , chat)
+                            chatList.append(chat)
+                        if str(chat) == 'ready':
+                            # stop_thread = True
+                            break
+                    
+                    background_thread.join()
+                    conn.send('ok'.encode())
+                    time.sleep(5)
+                # else :
+                #     print("lastChat is : " , lastChat)
+                #     if str(lastChat) != 'ready':
+                #         time.sleep(2)
+                #         conn.send('ok'.encode())
+                    
+                else :
+                    conn.send('ok'.encode())  
+                # send score board
+                time.sleep(5)
                 conn.send(str.encode(json.dumps(score_board)))
+                # conn.recv(1024).decode()
                 print(score_board)
-
+                time.sleep(5)
             # bread the spin lock after quiz ended
             break
 
@@ -81,9 +145,10 @@ def progress(conn, addr, score_board):
 
 my_score_dict = dict()
 ThreadCount = 0
-
+listOfConnections = []
 while True:
     conn, address = server_socket.accept()
+    listOfConnections.append(conn)
     if address[1] not in my_score_dict:
         my_score_dict[address[1]] = 0
     print('Connected to: ' + address[0] + ':' + str(address[1]))
